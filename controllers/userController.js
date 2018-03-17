ccc5f8e30c122f46cddd3e59d2a5bfe4bf25d5f3
Buwa <bubay565@yahoo.com>
2018-03-17 20:40:38 +0000

    implement user login and account registration

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+exports.loginForm = (req, res) => {
+	res.render('login', { title: 'Login' });
+}
+
+exports.registerForm = (req, res) => {
+	res.render('register', { title: 'Register' });
+}
+
+exports.validateRegister = (req, res, next) => {
+	req.sanitizeBody('name');
+	req.checkBody('name', 'You must provide a name').notEmpty();
+	req.checkBody('email', 'The email you have  provided is invalid!').isEmail();
+	req.sanitizeBody('email').normalizeEmail({
+		remove_dots: false,
+		remove_extension: false,
+		gmail_remove_subaddress: false
+	});
+	req.checkBody('password', 'Password cannot be blank!').notEmpty();
+	req.checkBody('password-confirm', 'Confirmation Password cannot be blank!').notEmpty();
+	req.checkBody('password-confirm', 'Oops! Your passwords do not match!').equals(req.body.password);
+
+	const errors = req.validationErrors();
+	if (errors) {
+		req.flash('error', errors.map(err => err.msg));
+		res.render('register', {title: 'Register', body: req.body, flashes: req.flash() });
+
+		return;
+	}
+	next();
+};
