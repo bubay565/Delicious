d7d0ddd1888adb5d596734473b60bf058cef4d77
Buwa <bubay565@yahoo.com>
2018-05-18 07:10:30 +0100

    plot stores on custom google map

--- routes/index.js
+++ routes/index.js
@@ -56,10 +56,12 @@ router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token',
 authController.confirmPasswords, catchErrors(authController.update));
 
+router.get('/map', storeController.mapPage);
+
 /*
 	API
 */
 
 router.get('/api/search', catchErrors(storeController.searchStores));
-
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 module.exports = router;
